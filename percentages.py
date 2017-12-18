3e2cf2ee481d855fd74b5c5709db14b200b6aa3c
Chris Cole <c.cole@dundee.ac.uk>
2017-12-18 21:31:56 +0000

    percentages.py v0.2 adding shineDelgarno() output to report on S-D

--- percentages.py
+++ percentages.py
@@ -9,13 +9,14 @@ Calculate percentages of start codons
 import argparse
 import re
 
-version = '0.1'
+version = '0.2'
 
 def main(infile):
     """Main code block"""
     
     n = 0
     i = 0
+    sd = 0
     seq = ''
     f = open(infile, 'r')
     for line in f:
@@ -27,11 +28,38 @@ def main(infile):
                 pass
             if (re.match('ATG', line)):
                 i = i + 1
+                try:
+                    if (shineDelgarno(line)):
+                        sd = sd + 1
+                except Exception as e:
+                    print("Error at line {}: {}".format(n, e))
     
     print("Percent start codons: {:.2%}".format(i/n))
+    print("Number of Shine-Delgarno sequences: {}".format(sd))
+    print("\tas a % of seq with starts: {:.2%}".format(sd/i))
+    print("\tas a % of all seqs: {:.2%}".format(sd/n))
+
           
         
+def shineDelgarno(seq, n = 20):
+    """
+    Determine whether there is a Shine-Delgarno sequence within first 20bp
     
+    :params seq: input nucleotide sequence
+    :params n: first n bases to query (default: 20)
+    """
+    
+    # check seq is long enough
+    if (len(seq) < n):
+        raise Exception("Sequence is shorter than 20bp. Skipped.")
+    
+    # grab first n bases of sequence
+    subSeq = seq[:n]
+    
+    if (re.search('AGGAGG',subSeq)):
+        return(True)
+    else:
+        return(False)
     
 
 if __name__ == "__main__":
